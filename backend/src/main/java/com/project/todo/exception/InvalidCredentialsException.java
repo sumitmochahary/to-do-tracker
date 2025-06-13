9ffb20cbfc69e86f0102616c7c48b7e481ff913f
Sumit Mochahary <sumitmochahary5@gmail.com>
2025-06-14 02:13:45 +0530

    Refactor: Remove unused classes and interfaces
    - Deleted AppConfig.java, PasswordResetController.java, TaskController.java, UserController.java, Media.java, PasswordResetToken.java, Task.java, Users.java, and related exception classes.
    - Removed JwtFilter.java and EmailService related classes.
    - Eliminated repository interfaces for PasswordResetToken, Task, and User.
    - Cleaned up service interfaces and implementations for PasswordReset, Task, and User services.
    - Removed application.properties file and test classes.
    - This commit aims to streamline the codebase by removing obsolete components and improving maintainability.

--- backend/src/main/java/com/project/todo/exception/InvalidCredentialsException.java
+++
@@ -1,8 +0,0 @@
-package com.project.todo.exception;
-
-public class InvalidCredentialsException extends Exception {
-    public InvalidCredentialsException(String message) {
-        super(message);
-    }
-}
-
--- backend/src/main/java/com/project/todo/exception/TokenExpiredException.java
+++
@@ -1,7 +0,0 @@
-package com.project.todo.exception;
-
-public class TokenExpiredException extends RuntimeException {
-    public TokenExpiredException(String message) {
-        super(message);
-    }
-}
